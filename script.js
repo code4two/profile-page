914b1d31fdf894d2e036edbc0b4957c5acf40bc5
Peter Adepoju <peteradepoju22@gmail.com>
2025-08-28 20:52:01 +0100

    updated default values

--- script.js
+++ script.js
@@ -7,10 +7,16 @@ submitButton.addEventListener("click", () => {
   console.log("Submit Button Clicked!!🤗");
   console.log(`${data1.value} \n ${data2.value}`);
 
+  //   IF TRUE, OUTPUT THIS
+
   // DEFAULT OUTPUT AFTER SUBMIT BUTTON IS CLICKED
   dataEntered.innerHTML = `Name: ${data1.value} \ Email: ${data2.value}`;
 
   // IF VALIDATIIN REQUIREMENTS ARE MET = OUTPUT THIS
   submitButton.innerHTML = "Following ✅";
   submitButton.style.backgroundColor = "green";
+
+  //   //IF FALSE, OUTPUT
+  //   dataEntered.innerHTML = `**In your email box: avoid spacing and include @`;
+  //   dataEntered.style.color = "red";
 });
