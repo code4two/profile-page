8d8a4e3a0002d4ad59ac5161c5b65b5824c28ee7
Peter Adepoju <peteradepoju22@gmail.com>
2025-08-29 09:15:28 +0100

    initial form validation done

--- script.js
+++ script.js
@@ -7,20 +7,13 @@ submitButton.addEventListener("click", () => {
   console.log("Submit Button Clicked!!🤗");
   console.log(`${data1.value} \n ${data2.value}`);
 
-  if (data2.value) {
+  if (data2.value.includes(" ") || !data2.value.includes("@")) {
+    dataEntered.innerHTML = `**Enter email properly: avoid spacing and include @`;
+    dataEntered.style.color = "red";
+    data2.style.outline = "red";
   } else {
+    submitButton.innerHTML = "Following ✅";
+    submitButton.style.backgroundColor = "green";
+    dataEntered.innerHTML = `Name: ${data1.value} \ Email: ${data2.value}`;
   }
-
-  //   IF TRUE, OUTPUT THIS
-
-  // DEFAULT OUTPUT AFTER SUBMIT BUTTON IS CLICKED
-  dataEntered.innerHTML = `Name: ${data1.value} \ Email: ${data2.value}`;
-
-  // IF VALIDATIIN REQUIREMENTS ARE MET = OUTPUT THIS
-  submitButton.innerHTML = "Following ✅";
-  submitButton.style.backgroundColor = "green";
-
-  //   //IF FALSE, OUTPUT
-  //   dataEntered.innerHTML = `**In your email box: avoid spacing and include @`;
-  //   dataEntered.style.color = "red";
 });
